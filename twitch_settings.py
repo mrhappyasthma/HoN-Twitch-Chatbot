57a19c87c9c41402c7ec4599677f96f76f449c06
Mark Klara <mrhappyasthma@gmail.com>
2016-09-03 17:47:33 -0700

    Adding twitch settings fields.

--- twitch_settings.py
+++ twitch_settings.py
@@ -1,5 +1,8 @@
 # Twitch Settings.
 HOST = "irc.twitch.tv"
 PORT = 6667
+BOT_USERNAME = ""  # Enter the bot's username.
+CHANNEL = ""  # Enter your channel name.
 
 # Get from http://twitchapps.com/tmi/.
+OAUTH = ""
